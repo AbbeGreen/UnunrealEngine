c4ff2d1e2203f8e0311b17bbeac5f2989482c117
AbbeGreen <greenalbin99@gmail.com>
2023-02-09 14:58:21 +0100

    refactor

--- test.c
+++ test.c
@@ -27,6 +27,7 @@ int rectCount = 0;
 struct Point {
     int x;
     int y;
+    int z;
     char name;
 };
 
@@ -37,25 +38,26 @@ struct Tri {
     int c;
 };
 
-struct Point create_point(int x, int y, char name) {
+struct Point create_point(int x, int y, int z, char name) {
 
     struct Point p;
 
     p.x = x;
     p.y = y;
+    p.z = z;
     p.name = name;
 
     return p;
 
 }
 
-struct Tri create_tri(int x1, int y1, int x2, int y2, int x3, int y3, int c) {
+struct Tri create_tri(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3, int c) {
 
     struct Tri t;
 
-    t.p1 = create_point(x1, y1, 'a');
-    t.p2 = create_point(x2, y2, 'b');
-    t.p3 = create_point(x3, y3, 'c');
+    t.p1 = create_point(x1, y1, z1, 'a');
+    t.p2 = create_point(x2, y2, z2, 'b');
+    t.p3 = create_point(x3, y3, z3, 'c');
     t.c = c;
 
     return t;
@@ -83,34 +85,46 @@ int interpolate_y(struct Point p1, struct Point p2, int x) {
 
 }
 
+struct Point *sort_points(struct Point *points, char attr) {
+
+    struct Point *sorted = (struct Point*) malloc(sizeof(struct Point) * 3);
+
+    for (int i = 0; i < 3; i++) {
+        sorted[i] = points[i];
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (attr == 'x') {
+                if (sorted[j].x >= sorted[j+1].x) {
+                    struct Point tmp = sorted[j];
+                    sorted[j] = sorted[j+1];
+                    sorted[j+1] = tmp;
+                }
+            } else if (attr == 'y') {
+                if (sorted[j].y >= sorted[j+1].y) {
+                    struct Point tmp = sorted[j];
+                    sorted[j] = sorted[j+1];
+                    sorted[j+1] = tmp;
+                }
+            } else if (attr == 'z') {
+                if (sorted[j].z >= sorted[j+1].z) {
+                    struct Point tmp = sorted[j];
+                    sorted[j] = sorted[j+1];
+                    sorted[j+1] = tmp;
+                }
+            }
+        }
+    }
+    return sorted;
+}
+
 int rasterize_triangle(struct Tri tri, int offset) {
-    // define left- or right-facing triangle
-    struct Point a = tri.p1;
-    struct Point b = tri.p2;
-    struct Point c = tri.p3;
 
-    struct Point x_sorted[3] = {a, b, c};
-    struct Point y_sorted[3] = {a, b, c};
+    struct Point to_sort[3] = {tri.p1, tri.p2, tri.p3};
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            if (x_sorted[j].x >= x_sorted[j+1].x) {
-                struct Point tmp = x_sorted[j];
-                x_sorted[j] = x_sorted[j+1];
-                x_sorted[j+1] = tmp;
-            }    
-        }
-    }
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            if (y_sorted[j].y >= y_sorted[j+1].y) {
-                struct Point tmp = y_sorted[j];
-                y_sorted[j] = y_sorted[j+1];
-                y_sorted[j+1] = tmp;
-            }    
-        }
-    }
+    struct Point *x_sorted = sort_points(to_sort, 'x');
+    struct Point *y_sorted = sort_points(to_sort, 'y');
 
     SDL_Surface *window_surface = SDL_GetWindowSurface(screen);
     unsigned int *pixels = window_surface->pixels;
@@ -126,7 +140,7 @@ int rasterize_triangle(struct Tri tri, int offset) {
     middle = &y_sorted[1];
     bottom = &y_sorted[2];
 
-    right_facing = middle->x < bottom->x;
+    right_facing = middle->x <= top->x;
 
     for (int i = top->y; i < middle->y; i += offset) {
         if (right_facing) {
@@ -140,7 +154,7 @@ int rasterize_triangle(struct Tri tri, int offset) {
             to = interpolate_x(*top, *middle, i);
         }
         for (int o = 0; o < offset; o++) {
-            for (int j = from; j < to; j++) {
+            for (int j = from; j <= to; j++) {
                 pixels[j + (i + o) * width] = tri.c;
             }
         }
@@ -157,7 +171,7 @@ int rasterize_triangle(struct Tri tri, int offset) {
             to = interpolate_x(*middle, *bottom, i);
         }
         for (int o = 0; o < offset; o++) {
-            for (int j = from; j < to; j++) {
+            for (int j = from; j <= to; j++) {
                 pixels[j + (i + o) * (width)] = tri.c;
             }
         }
@@ -165,6 +179,23 @@ int rasterize_triangle(struct Tri tri, int offset) {
 
     return 0;
 
+}
+
+void render(struct Tri *tris, int n_tris) {
+
+    // DO RENDERING THINGS
+
+    // SET UP SURFACE AND DEFINE WINDOW PARAMS
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(screen);
+    unsigned int *pixels = window_surface->pixels;
+    int width = window_surface->w;
+
+    int offset = 1; //pixel size
+
+    for (int i = 0; i < n_tris; i++) {
+        rasterize_triangle(tris[i], offset);
+    }
 }
 
 
@@ -184,31 +215,22 @@ void finish() {
     exit(0);
 }
 
-void draw_rectangle() {
-    SDL_RenderClear(renderer);
-    char buff[20];
-    SDL_Rect rect;
-    rect.h = 120;
-    rect.w = 120;
-    rect.y = HEIGHT/2;
-    rect.x = WIDTH/2;
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &rect);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderPresent(renderer);
-}
-
-void game_loop(struct Tri tris[], int tris_size) {
+void game_loop() {
 
     int game_running = 1;
 
+    struct Tri tri = create_tri(400, 200, 0, 200, 400, 0, 400, 400, 0, 0xff00ffff);
+    struct Tri tri2 = create_tri(200, 400, 0, 400, 400, 0, 400, 600, 0, 0x00ff00ff);
+    struct Tri tri3 = create_tri(400, 400, 0, 400, 600, 0, 600, 400, 0, 0xffff0000);
+    struct Tri tri4 = create_tri(400, 200, 0, 400, 400, 0, 600, 400, 0, 0xff00ff00);
+    struct Tri tris[4] = {tri, tri2, tri3, tri4};
+
+    int tris_size = sizeof(tris) / sizeof(tris[0]);
+
     while (game_running) {
+
+        render(tris, tris_size);
         //draw_rectangle();
-        for (int i = 0; i < tris_size; i++) {
-
-            rasterize_triangle(tris[i], 1);
-
-        }
         SDL_UpdateWindowSurface(screen);
         
         while (SDL_PollEvent(&event)) {
@@ -231,17 +253,9 @@ void game_loop(struct Tri tris[], int tris_size) {
 }
 
 int main(int argc, char **arg) {
-    
-    //printf("hello");
-    struct Tri tri = create_tri(100, 100, 300, 100, 100, 300, 0xff00ffff);
-    struct Tri tri2 = create_tri(100, 200, 500, 300, 400, 400, 0x00ff00ff);
-    struct Tri tri3 = create_tri(400, 400, 500, 500, 400, 500, 0xffff0000);
-    struct Tri tris[3] = {tri, tri2, tri3};
-
-    int tris_size = sizeof(tris) / sizeof(tris[0]);
 
     init_setup();
-    game_loop(tris, tris_size);
+    game_loop();
     finish();
     return 0;
 }
